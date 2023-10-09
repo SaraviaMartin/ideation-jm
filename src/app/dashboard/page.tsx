d11c29fdb8f503c6853a71f406445dee8c162485
Juan <saraviamartin16@hotmail.com>
2023-10-09 16:36:37 -0300

    Create Note component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import {ArrowLeft} from "lucide-react"
 import { UserButton } from '@clerk/nextjs'
 import { Separator } from '@/components/ui/separator'
+import CreateNoteDialog from '@/components/CreateNoteDialog'
 
 type Props = {}
 
@@ -39,6 +40,11 @@ const DashboardPage = (props: Props) => {
             <h2 className='text-xl text-gray-500'>You have no notes yet</h2>
         </div>
 
+
+      {/* display allthe notes  */}
+      <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-3">
+        <CreateNoteDialog/>
+      </div>
 
       </div>
     </div>
