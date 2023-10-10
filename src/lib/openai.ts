4d6d7be9ec2633796a028b991c108b48f128ecdd
Juan <saraviamartin16@hotmail.com>
2023-10-10 18:11:30 -0300

    error create NoteBook

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -14,7 +14,7 @@ export async function generateImagePrompt(name: string) {
             messages: [
                 {
                     role: 'system',
-                    content: 'You are an creative and helpful AI assistance capable of generating interesting thumbnail descrtiption for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled'
+                    content: "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled"
                 },
                 {
                     role: 'user',
@@ -23,14 +23,24 @@ export async function generateImagePrompt(name: string) {
             ],
         });
         const data = await response.json()
-        const image_description = data.choices[0].message.content
+        const image_description = data.choices[0].message.content;
         return image_description as string
     } catch (error) {
         console.log(error)
-        throw error;
     }
 }
 
-export async function generateImage() {
-
+export async function generateImage(image_description: string) {
+    try {
+        const response = await openai.createImage({
+            prompt: image_description,
+            n: 1,
+            size: '256x256'
+        })
+        const data  = await response.json()
+        const image_url = data.data[0].url
+        return image_url as string
+    } catch (error) {
+        console.error(error)
+    }
 }
