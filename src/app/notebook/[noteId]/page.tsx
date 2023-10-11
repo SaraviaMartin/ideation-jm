50ec25bb0335af680d5f11d76af1d5faf99680ea
Juan <saraviamartin16@hotmail.com>
2023-10-11 10:49:16 -0300

    notebookPage

---
+++ src/app/notebook/[noteId]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+    params:{
+        noteId: string
+    }
+}
+
+const NotebookPage = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NotebookPage
